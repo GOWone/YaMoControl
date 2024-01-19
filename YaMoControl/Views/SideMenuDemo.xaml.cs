eac720182684c8813978f099e2b8221933a6284d
亚 陈 <2227237581@qq.com>
2024-01-19 23:26:30 +0800

    代码优化

--- YaMoControl/Views/SideMenuDemo.xaml.cs
+++ YaMoControl/Views/SideMenuDemo.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using YaMoControl.Models;
 
 namespace YaMoControl.Views
 {
@@ -19,9 +21,24 @@ namespace YaMoControl.Views
     /// </summary>
     public partial class SideMenuDemo : Window
     {
+        public ObservableCollection<ListItemModel> nameList = new ObservableCollection<ListItemModel>();
         public SideMenuDemo()
         {
             InitializeComponent();
+            init();
+            listTemp.ItemsSource = nameList;
+        }
+
+        public void init()
+        {
+            nameList.Add(new ListItemModel() {Index = 1, Name="123", Remark= "123"});
+            nameList.Add(new ListItemModel() {Index = 1, Name="123", Remark= "123"});
+            nameList.Add(new ListItemModel() {Index = 1, Name="123", Remark= "123"});
+            nameList.Add(new ListItemModel() {Index = 1, Name="123", Remark= "123"});
+            nameList.Add(new ListItemModel() {Index = 1, Name="123", Remark= "123"});
+            nameList.Add(new ListItemModel() {Index = 1, Name="123", Remark= "123"});
+            nameList.Add(new ListItemModel() {Index = 1, Name="123", Remark= "123"});
+            nameList.Add(new ListItemModel() {Index = 1, Name="123", Remark= "123"});
         }
     }
 }
