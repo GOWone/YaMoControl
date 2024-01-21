5ee88cd09e164ec76b83440a30e78228ea38c09c
亚 陈 <2227237581@qq.com>
2024-01-21 18:13:04 +0800

    代码优化

---
+++ YaMoControlDesign/Controls/Input/TextBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+using YaMoControlDesign.Interactivity;
+
+namespace YaMoControlDesign.Controls
+{
+
+    public class TextBox : System.Windows.Controls.TextBox
+    {
+        public TextBox()
+        {
+            CommandBindings.Add(new CommandBinding(ControlCommands.Clear, (s, e) =>
+            {
+                if (IsReadOnly)
+                {
+                    return;
+                }
+
+                SetCurrentValue(TextProperty, string.Empty);
+            }));
+        }
+    }
+}
